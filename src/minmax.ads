84c8aca4ced21c5a29ecc2b74dca23815ffd5e58
themrb <366838+themrb@users.noreply.github.com>
2012-03-18 21:38:45 +1100

    forced move check function

--- src/minmax.ads
+++ src/minmax.ads
@@ -6,5 +6,6 @@ package MinMax is
    procedure Min (state : in out GameTree_Type);
    procedure Max (state : in out GameTree_Type);
    function Terminal(state : in State_Type) return Boolean;
+   function Forced_Move_Check (state : in GameTree_Type) return GameTree_Type;
 
 end MinMax;
