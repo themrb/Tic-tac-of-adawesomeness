4b993fce70f2b4954139749a5e5207bda936b8fe
themrb <366838+themrb@users.noreply.github.com>
2012-03-19 18:41:29 +1100

    further work on players

--- src/players.adb
+++ src/players.adb
@@ -2,14 +2,28 @@
 package body Players is
 
    task body Player is
+      state : State_Type;
+      chosenmove : Place;
    begin
       accept Initialise;
 
       loop
-         accept Next_Move(previous : in State_Type, next : out Place)
-
+         accept Next_Move(previous : in State_Type) do
+            state := previous;
          end Next_Move;
+
+         declare
+            GameTreeRoot : GameTree_Type;
+         begin
+            GameTreeRoot.state := state;
+            Max(GameTreeRoot);
+            chosenmove := GameTreeRoot.best.state.spot;
+         end;
+
+         accept Choose_Move(next : out Place) do
+            next := chosenmove;
+         end Choose_Move;
       end loop;
-   end Player
+   end Player;
 
 end Players;
