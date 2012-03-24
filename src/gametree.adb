31a7b8d87d38b215daa084515a7c4dd1c3bc70b4
Josh Godsiff <thedataangel@gmail.com>
2012-03-25 00:22:08 +1100

    Got expansion ordering working

--- src/gametree.adb
+++ src/gametree.adb
@@ -6,38 +6,57 @@ package body GameTree is
 
    function Expand(state : in GameTree_Type) return ExpandedChildren is
       temp : aliased GameTree_Type;
-      Childrens : ExpandedChildren;
-      Child_Counter : Children_Range := Children_Range'First;
+      Children : ExpandedChildren;
+      frontCounter, backCounter : Children_Range;
    begin
---        temp.parent := new GameTree_Type'(state);
+      frontCounter := Children_Range'First;
+      backCounter := Children_Range'Last - Children_Range(state.state.turns + 1) + 1;
+
       for i in Dimension'Range loop
          for j in Dimension'Range loop
             for k in Dimension'Range loop
-               if(state.state.current_stateX(i,j,k) = False and state.state.current_stateO(i,j,k) = False) then
+               if(not state.state.current_stateX(i,j,k) and then not state.state.current_stateO(i,j,k)) then
                   temp := state;
                   temp.state.justWent := NextPlayer(state.state.justWent);
+
                   if (temp.state.justWent = X) then
                      temp.state.current_stateX(i,j,k) := True;
                   elsif (temp.state.justWent = O) then
                      temp.state.current_stateO(i,j,k) := True;
                   end if;
 
---                    temp.state.current_state(i,j,k) := temp.state.justWent;
                   temp.state.spot := (i,j,k);
                   temp.state.turns := state.state.turns + 1;
---                    Put_Line(Image(temp.state));
---                  Prepend(The_List, temp);
---                    Put_Line(Child_Counter'Img);
-                  Childrens(Integer(Child_Counter)) := temp;
-                  if (Integer(Child_Counter) /= 64) then
-                     Child_Counter := Child_Counter + 1;
+
+                  if((temp.state.turns) /= state.state.turns + 1) then
+                     Put_Line(temp.state.turns'Img & state.state.turns'Img);
                   end if;
+
+                  -- Check if corner or inner nodes, and if so, add to front of
+                  -- queue
+                  if(((i = 0 or i = 3) and then (j = 0 or j = 3) and then (k = 0 or k = 3)) or else
+                    ((i = 1 or i = 2) and then (j = 1 or j = 2) and then (k = 1 or k = 2))) then
+                     Children(frontCounter) := temp;
+                     if(frontCounter < Children_Range'Last) then
+                        frontCounter := frontCounter + 1;
+                     end if;
+                  else -- Otherwise, back of the line!
+                     Children(backCounter) := temp;
+                     if(backCounter > Children_Range'First) then
+                        backCounter := backCounter - 1;
+                     end if;
+                  end if;
+
+--                    Children(frontCounter) := temp;
+--                    if(frontCounter < Children_Range'Last) then
+--                       frontCounter := frontCounter + 1;
+--                    end if;
                end if;
             end loop;
          end loop;
       end loop;
 
-      return Childrens;
+      return Children;
    end Expand;
 
 end GameTree;
