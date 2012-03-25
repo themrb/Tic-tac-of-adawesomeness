e3838d7936a3fa2f6292a630b836f480640c1c79
Josh Godsiff <thedataangel@gmail.com>
2012-03-26 01:19:39 +1100

    Commented players

--- src/players.ads
+++ src/players.ads
@@ -4,19 +4,29 @@ with GameTree; use GameTree;
 
 package Players is
 
-
+   -- Task representing both of the players.
+   -- Used only for setting up the computations prior to each move, and
+   -- then retrieving the result once computations are done.
    task type Player (name : Cell) is
+      -- Gets the move that just happened.
       entry Next_Move(previous : in State_Type);
+      -- Gives the player's choice of move.
       entry Choose_Move(next : out Place);
       entry Shutdown;
    end Player;
 
    type Player_Access is access all Player;
 
+   -- Protected object which manages data about and including the set of nodes
+   -- currently being explored, or which are going to be explored in this pass.
    protected type BeingExplored is
+      -- Allows explorer task to get the next node to be explored.
       entry Next(node, parent : out GameTree_Type; a, b : out BoardValue);
+      -- Allows player task to retrieve the result of exploring the game-tree.
       entry GetResult(node : out GameTree_Type);
+      -- Allows explorer tasks to report what they found.
       procedure Report(board, parent : in GameTree_Type; bValue : in BoardValue);
+      -- Initialise the set to explore. Note that we do this for every turn.
       procedure Initialise (parent : in GameTree_Type);
    private
       root, best : GameTree_Type;
@@ -24,14 +34,14 @@ package Players is
       index : Children_Range := Children_Range'First;
       more : Boolean := False; -- Any more boards to check?
       alpha, beta, value : BoardValue;
-      goNuts, checkNuts : Boolean := True; --Allow everyone to compute.
+      goNuts, checkNuts : Boolean := True; --Switches which allow just one worker,
+                                           -- or all to compute.
 
-      procedure CheckForTerminals;
+      procedure CheckForTerminals;         -- Look ahead to see if we have any terminals.
    end BeingExplored;
 
+   -- Task which actually explores the game-tree.
    task type Explorer(toExplore : access BeingExplored) is
    end Explorer;
-
---     type Explorers is array(Natural range 1..8) of Explorer;
 
 end Players;
