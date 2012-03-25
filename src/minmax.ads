b81848c31f1b1ce3016861b614e7c16fb70f8081
Josh Godsiff <thedataangel@gmail.com>
2012-03-25 18:51:14 +1100

    Concurrency! May still be a few problems with it, but it functions

--- src/minmax.ads
+++ src/minmax.ads
@@ -3,9 +3,9 @@ with Boards; use Boards;
 
 package MinMax is
 
-   procedure Min (state : in out GameTree_Type;  depth : in  TurnsNo; outValue : out BoardValue; best : out GameTree_Type;
+   procedure Min (state : in out GameTree_Type;  depth : in  TurnsNo; outValue : out BoardValue;
                   alpha, beta : in BoardValue);
-   procedure Max (state : in out GameTree_Type;  depth : in  TurnsNo; outValue : out BoardValue; best : out GameTree_Type;
+   procedure Max (state : in out GameTree_Type;  depth : in  TurnsNo; outValue : out BoardValue;
                   alpha, beta : in BoardValue);
    --function Forced_Move_Check (state : in GameTree_Type) return GameTree_Type;
 
--- src/players.ads
+++ src/players.ads
@@ -4,10 +4,32 @@ with GameTree; use GameTree;
 
 package Players is
 
+
    task type Player (name : Cell) is
-      entry Initialise;
       entry Next_Move(previous : in State_Type);
       entry Choose_Move(next : out Place);
    end Player;
+
+   type Player_Access is access all Player;
+
+   protected type BeingExplored is
+      entry Next(node, parent : out GameTree_Type; a, b : out BoardValue);
+      entry GetResult(node : out GameTree_Type);
+      procedure Report(board, parent : in GameTree_Type; bValue : in BoardValue);
+      procedure Initialise (parent : in GameTree_Type);
+   private
+      root, best : GameTree_Type;
+      boards : ExpandedChildren;
+      index : Children_Range := Children_Range'First;
+      more : Boolean := False; -- Any more boards to check?
+      alpha, beta, value : BoardValue;
+
+      procedure CheckForTerminals;
+   end BeingExplored;
+
+   task type Explorer(toExplore : access BeingExplored) is
+   end Explorer;
+
+--     type Explorers is array(Natural range 1..8) of Explorer;
 
 end Players;
