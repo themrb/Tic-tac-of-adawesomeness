9e32a7c7afd175d4eda684e2b017a095b8d46a7d
themrb <366838+themrb@users.noreply.github.com>
2012-03-26 02:01:38 +1100

    added timings

--- src/configure.ads
+++ src/configure.ads
@@ -4,7 +4,7 @@ package Configure is
    -- Total depth to search to
    depth : TurnsNo := 5;
    -- Number of worker tasks: Warning, should be set to one less than number of available cores
-   workerTasks : Natural := 3;
+   workerTasks : Natural := 1;
 
    count : Natural := 0;
 end Configure;
